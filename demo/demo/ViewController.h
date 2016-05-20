b6fbca76e4c42b824da03a58d0066b75f7f9a4f3
zoujinglong <959342345.com>
2016-05-20 13:56:04 +0800

    初始化项目

---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by zjl on 16/5/20.
+//  Copyright © 2016年 zjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by zjl on 16/5/20.
+//  Copyright © 2016年 zjl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"初始化项目");
+}
+
+@end
